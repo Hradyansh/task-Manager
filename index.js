a1b15b72c490c0cee26bc79f98ad68d0a056ac34
HradyansH <43550510+Hradyansh@users.noreply.github.com>
2022-03-06 21:36:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import "./styles.css";
+
 //IMPORTS
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const showUserButton = document.querySelector(".showUserButton");
